d1b425ea84512613b90e7cc5a0f9478c87c9022b
Kyeongrok Kim <oceanfog1@gmail.com>
2021-04-21 17:02:17 +0900

    상세 분류 추가

--- 02_label.py
+++ 02_label.py
@@ -8,45 +8,51 @@ def get_person(text):
 
     if '4인용' in text:
         return 4
+    elif '4인 식탁세트' in text:
+        return 4
     elif '2인용' in text:
         return 2
     else:
-        return 0
+        return None
 
 def get_color(text):
     if '네츄럴' in text:
-        return '네츄럴'
+        return 0
     elif '브라운' in text:
-        return '브라운'
+        return 1
     else:
-        return '없음'
+        return None
 
 def get_type(text):
-    text = '선택1: 4인용 식탁세트 / 선택2: 네츄럴 / 선택3: 체어형(의자4개)'
     if '체어형' in text:
-        return '체어형'
+        return 0
     elif '벤치형' in text:
-        return '벤치형'
+        return 1
     elif '심플형' in text:
-        return '심플형'
+        return 2
     elif '쿠션형' in text:
-        return '쿠션형'
+        return 3
     else:
-        return '없음'
+        return None
 
 def get_sel_1(text):
     if '의자4개' in text:
-        return '의자4개'
-    elif '벤치2개' in text:
-        return '벤치2개'
-    elif '벤치1+의자2' in text:
-        return '벤치1+의자2'
-    elif '벤치1개 + 의자2개' in text:
-        return '벤치1+의자2'
-    elif '벤치1개+의자2개' in text:
-        return '벤치1+의자2'
-    else:
         return 0
+    elif '벤치2개' in text:
+        return 1
+    elif '벤치1+의자2' in text:
+        return 2
+    elif '벤치1개 + 의자2개' in text:
+        return 2
+    elif '벤치1개+의자2개' in text:
+        return 2
+    elif '2인테이블+의자2개' in text:
+        return 3
+    elif '테이블+의자2개' in text:
+        return 3
+    # 4는 의자4개
+    else:
+        return None
 
 
 
@@ -56,15 +62,21 @@ lines = []
 with open('ff.txt') as f:
     lines = f.readlines()
 
-
+print('인용','색상', '타입', '선택1')
 for text in lines:
     use_persone = get_person(text)
     color = get_color(text)
     type1 = get_type(text)
     sel1 = get_sel_1(text)
-    if sel1 != 0 and use_persone == 0:
+    if sel1 != None and use_persone == None:
         use_persone = 4
+    if use_persone == 4 and type1 == 1 and sel1 == None:
+        sel1 = 2
 
     r = {'origin':'', 'use_person':use_persone, 'color':color, 'type1':type1, 'sel1':sel1}
-    print(text.replace('\n', ''))
-    print(r)
+    r2 = [use_persone, color, type1, sel1]
+    # print(text.replace('\n', ''))
+    if None in r2:
+        print(text.replace('\n', ''))
+    print(r2)
+
